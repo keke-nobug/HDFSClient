9996b210558627e90129ec6671a13c3df1076010
keke <934420809@qq.com>
2023-11-04 14:07:05 +0800

    commit

---
+++ src/main/java/com/atguigu/Demo01.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+/**
+ * @BelongsProject: HDFSClient
+ * @BelongsPackage: com.atguigu
+ * @Author: qzk
+ * @CreateTime: 2023/11/4 14:06
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("测试新建或修改代码，然后上传到Git");
+    }
+}
